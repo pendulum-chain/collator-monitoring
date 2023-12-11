a64492f3c2a611fe50b66688d7ba25df5493e574
bogdanS98 <bogdan.sandu13@gmail.com>
2023-12-11 22:44:40 +0200

    Update js file

--- src/monitorCollators.js
+++ src/monitorCollators.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const graphql_request_1 = require("graphql-request");
-// GraphQL Endpoint
+// GraphQL endpoint
+// Will be added as an env variable in the future
 const endpoint = 'https://squid.subsquid.io/foucoco-squid/graphql';
-// GraphQL Query (dynamic timestamp will be injected)
+// GraphQL query
 const getQuery = (timestamp) => (0, graphql_request_1.gql) `
     {
         blocks(
@@ -17,7 +18,7 @@ const getQuery = (timestamp) => (0, graphql_request_1.gql) `
         }
     }
 `;
-// Function to fetch blocks
+// Fetch blocks
 async function fetchBlocks(timestamp) {
     try {
         const query = getQuery(timestamp);
@@ -30,25 +31,25 @@ async function fetchBlocks(timestamp) {
         throw error;
     }
 }
-// Function to identify inactive collators
+// Identify inactive collators
 async function identifyInactiveCollators() {
     try {
         // Calculate the timestamp for 24 hours ago
         const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString();
         const blocks = await fetchBlocks(oneDayAgo);
         // Track block production
-        const collatorProduction = {};
+        const blockProduction = {};
         blocks.forEach(block => {
             const { validator, timestamp } = block;
-            collatorProduction[validator] = collatorProduction[validator] || [];
-            collatorProduction[validator].push(timestamp);
+            blockProduction[validator] = blockProduction[validator] || [];
+            blockProduction[validator].push(timestamp);
         });
         // Determine inactivity
-        const inactiveCollators = Object.keys(collatorProduction).filter(validator => {
-            const timestamps = collatorProduction[validator];
+        const inactiveCollators = Object.keys(blockProduction).filter(validator => {
+            const timestamps = blockProduction[validator];
             // Find the most recent timestamp for the validator
             const lastProductionTime = timestamps.reduce((latest, current) => latest > current ? latest : current, '');
-            return lastProductionTime > oneDayAgo;
+            return lastProductionTime < oneDayAgo;
         });
         console.log('Inactive collators:', inactiveCollators);
     }
@@ -56,8 +57,4 @@ async function identifyInactiveCollators() {
         console.error('Error identifying inactive collators:', error);
     }
 }
-// Execute
 identifyInactiveCollators();
-//slow collators alert or missing collator alert
-// polkadot js 
-// chains state -> session -> validaotrs

eb4591ccd89c5cf62353b03c0ece992b5bceceed
Marcel Ebert <mail@marcel-ebert.de>
2023-12-13 17:12:36 +0100

    Add `dev` script

--- src/slack.ts
+++ src/slack.ts
@@ -2,10 +2,10 @@ import { Block, KnownBlock } from '@slack/types';
 import axios from 'axios';
 
 if (!process.env.SLACK_WEB_HOOK_TOKEN) {
-    throw new Error('Slack webhook token is not defined.');
+    throw new Error('SLACK_WEB_HOOK_TOKEN is not defined.');
 }
 
-const slackWebHookUrl = 'https://hooks.slack.com/services/${process.env.SLACK_WEB_HOOK_TOKEN}';
+const slackWebHookUrl = `https://hooks.slack.com/services/${process.env.SLACK_WEB_HOOK_TOKEN}`;
 
 export type SlackBlock = KnownBlock | Block;
 
@@ -16,6 +16,7 @@ export interface SlackBlockkitMessage {
 export async function sendSlackNotification(message: SlackBlockkitMessage) {
     try {
         const payload = JSON.stringify(message);
+        console.log("Sending slack message: ", payload, slackWebHookUrl)
         await axios.post(slackWebHookUrl, payload, {
             headers: {
               "Content-Type": "application/json"
